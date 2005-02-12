1c9facd1f3bcb392832845f5cbf8c2a925d7d275
rip7 <rip7>
2005-02-12 18:26:26 +0000

    Delete functions maked and errors fixed

--- Yabe-Server/include/linkedlist.h
+++ Yabe-Server/include/linkedlist.h
@@ -30,8 +30,7 @@ template < class Type > class LinkedList
 	Type *AddItem ();
 	Type *Advance ();
 	Type *Rewind ();
-	Type *SetToHead ();
-	bool DeleteItem (Type * pDelItem);
+	bool DeleteCurrentPtr ();
 };
 
 template < class Type > LinkedList < Type >::LinkedList ():Size (0)
@@ -90,6 +89,42 @@ template < class Type > Type * LinkedList < Type >::Advance ()
 	}
 	pCurrent = pHead;
 	return pCurrent;
+}
+
+template < class Type > bool LinkedList < Type >::DeleteCurrentPtr ()
+{
+	Type *pTemp;
+
+	if (pCurrent == NULL)
+		return 0;
+	else
+	{ //indien Ptail = Phead dan blijft tail wel nog op oude waarde staan !
+		if (pCurrent == pHead)	//Case 1 delete = Head
+		{
+			pTemp = pHead;
+			pHead = pHead->Next;
+			delete pTemp;
+			pCurrent = pHead;
+			return 1;
+		}
+		else if (pCurrent == pTail)	//Case 2 delete is at the end
+		{
+			pTemp = pTail;
+			pTail = Rewind ();
+			pTail->Next = NULL;
+			delete pTemp;
+			pCurrent = pTail;
+			return 1;
+		}
+		else		//Case 3 delete is in middle somewhere
+		{
+			pTemp = pCurrent;
+			pCurrent = Rewind ();
+			pCurrent->Next = pTemp->Next;
+			delete pTemp;
+			return 1;
+		}
+	}
 }
 
 template < class Type > Type * LinkedList < Type >::GetCurrentPtr ()
